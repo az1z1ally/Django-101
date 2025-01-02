6fbb3697b8dd6454f2721a65bb339291a5527f81
az1z1ally <albyally5@gmail.com>
2025-01-03 00:32:09 +0300

    Create a user app and connect it to the project

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -23,7 +23,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'))
+    path('', include('users.urls')),
+    path('projects/', include('projects.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
